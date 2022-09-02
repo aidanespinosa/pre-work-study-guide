8e99cf644c4b6212384dd9e9ef8b90355ba24fef
aidanespinosa <aidanespinosa3@gmail.com>
2022-09-02 12:51:10 -0700

    adding js for loop

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,34 @@
-var topics = "HTML, CSS, GIT, Java Script";
-console.log(topics);
+var topics = ["HTML", "CSS","GIT", "Java Script" ];
+
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+
+var topic = ('HTML');
+
+if (topic === 'HTML') {
+    console.log("Let's Study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let/s Study CSS!");
+} else if (topic === 'GIT') {
+    console.log("Let's Study GIT!");
+} else if (topic === 'Java Script') {
+    console.log("Let's Study Java Script!");
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
